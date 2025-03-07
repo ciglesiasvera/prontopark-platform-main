946135d1271c9dfe32fc43295f6574a2c8c87c33
Cristian Iglesias Vera <ciglesiasvera@gmail.com>
2025-03-07 13:44:33 -0300

    update allowed host on settings

--- pronto_park/settings.py
+++ pronto_park/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('dprontopark-platform-main.onrender.com','DJANGO_ALLOWED_HOSTS', '').split(',')
+#ALLOWED_HOSTS = os.getenv('prontopark-platform-main.onrender.com','DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['prontopark-platform-main.onrender.com','127.0.0.1']
 
 
 # Application definition
